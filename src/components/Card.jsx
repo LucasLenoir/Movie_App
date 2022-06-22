7c07f4e00a7c95cdd2941b05ef4de65312a9f15a
LucasLenoir <95701721+LucasLenoir@users.noreply.github.com>
2022-06-22 10:15:11 +0200

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Card = ({ props }) => {
   return (
     <div className="card">
-      <h2>{props.title}</h2>
+      <h2>{props.title ?? props.name}</h2>
+      <h3>{props.results ? props.results : props.cast}</h3>
     </div>
   );
 };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ import Card from "./Card";
 const Form = () => {
   const [moviesData, setMoviesData] = useState([]);
   const [search, setSearch] = useState("");
-  const [searchArtist, setSearchArtist] = useState("");
+  // const [searchArtist, setSearchArtist] = useState("");
 
   useEffect(() => {
     axios
@@ -19,51 +19,68 @@ const Form = () => {
     if (search !== "") {
       axios
         .get(
-          `https://api.themoviedb.org/3/search/movie?api_key=06aed854c0bb71522c688e9d7119e01a&query=${search}`
+          `https://api.themoviedb.org/3/search/multi?api_key=06aed854c0bb71522c688e9d7119e01a&query=${search}`
         )
-        .then((res) => setMoviesData(res.data.results));
-    }
-  }, [search]);
-  useEffect(() => {
-    if (searchArtist !== "") {
-      axios
-        .get(
-          `https://api.themoviedb.org/3/search/person?api_key=06aed854c0bb71522c688e9d7119e01a&query=${searchArtist}`
-        )
-        .then((res) => res.data.results[0].id)
-        .then((id) => {
-          axios
-            .get(
-              `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=06aed854c0bb71522c688e9d7119e01a`
-            )
-            .then((res) => setMoviesData(res.data.cast));
+        .then((res) => {
+          console.log(res);
+          setMoviesData([]);
+          if (res.data.results) {
+            res.data.results.forEach((movie) => {
+              movie.media_type !== "person"
+                ? setMoviesData((movies) => {
+                    const clone = movies;
+                    clone.push(movie);
+                    return clone;
+                  })
+                : axios
+                    .get(
+                      `https://api.themoviedb.org/3/person/${movie.id}/movie_credits?api_key=06aed854c0bb71522c688e9d7119e01a`
+                    )
+                    .then((res) => {
+                      setMoviesData(res.data.cast);
+                    });
+            });
+          }
         });
     }
-  }, [searchArtist]);
-
+  }, [search]);
+  // useEffect(() => {
+  //   if (searchArtist !== "") {
+  //     axios
+  //       .get(
+  //         `https://api.themoviedb.org/3/search/person?api_key=06aed854c0bb71522c688e9d7119e01a&query=${searchArtist}`
+  //       )
+  //       .then((res) => res.data.results[0].id)
+  //       .then((id) => {
+  //         axios
+  //           .get(
+  //             `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=06aed854c0bb71522c688e9d7119e01a`
+  //           )
+  //           .then((res) => setMoviesData(res.data.cast));
+  //       });
+  //   }
+  // }, [searchArtist]);
   return (
     <div className="form-component">
       <div className="form-container">
-        <div className="input-container">
-          <form>
-            <input
-              type="text"
-              placeholder="Enter a movie name"
-              id="search-input"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            {/* <input type="submit" value="Research" /> */}
-          </form>
-          <form>
-            <input
+        <form>
+          <input
+            type="text"
+            placeholder="Enter a movie name"
+            id="search-input"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          {/* <input type="submit" value="Research" /> */}
+        </form>
+        <form>
+          {/* <input
               type="text"
               placeholder="Enter an actor name"
               id="search-input"
               onChange={(e) => setSearchArtist(e.target.value)}
-            />
-            {/* <input type="submit" value="Research" /> */}
-          </form>
-        </div>
+            /> */}
+          {/* <input type="submit" value="Research" /> */}
+        </form>
 
         <div className="btn-sort-container">
           <div className="btn-sort" id="goodToBad">
